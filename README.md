308dc36356a4822f6d9bb22fe33f01939bd16959
lorenzo3117 <40773537+lorenzo3117@users.noreply.github.com>
2019-07-10 16:56:25 +0200

    Added comments and an error catching function

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # weather-app
-*A weather app made in JavaScript*
+A simple weather app made in JavaScript using the https://openweathermap.org/ api
 
 ## To do
 - Change table and button position so they stay in place DONE
+- Catch any errors while fetching so .then method aren't executed DONE
 - Reset order
 - Remove city button
 - Change order
--- script.js
+++ script.js
@@ -1,36 +1,69 @@
 //Weather app
 function weather(city) {
+    // Prompt + general information
     var city = prompt("Which city?");
     const api = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=02587cc48685af80ea225c1601e4f792&units=metric";
     var warning = document.querySelector("#warningMessage");    
 
-    fetch(api)        
+    // Fetch the api
+    fetch(api)
+
+        // Check if there are any errors with the function handleErrors
+        .then(handleErrors)
+
+        // If the are no errors, get JSON response from api
         .then(function (weatherResponse) {
             return weatherResponse.json()
         })
+
+        // Use api data to change website
         .then(function (weatherData) {
+            // Removes warning message
             warning.style.display = "none";
 
+            // Puts the JSON into an array and launch createTable function
             var arrayJSON = [weatherData];
             createTable(document.querySelector("#table"), arrayJSON);
 
+            // Function to create the table
             function createTable(table, data) {
+                // Makes table visible and changes button text
                 document.querySelector("#table").style.display = "block";
                 document.querySelector("#cityButton").innerHTML = "Add city";
                 
+                // Goes through array and makes the rows for the table
                 for (var i = 0; i < data.length; i++) {
                     let rowData = data[i];
                     var row = table.insertRow(table.rows.length);
+                    // This var exist to make the first letter capitalized
                     var weatherDescription = rowData.weather[0].description;
+                    
                     row.insertCell(0).innerHTML = rowData.name;
                     row.insertCell(1).innerHTML = rowData.main.temp + " °C";
                     row.insertCell(2).innerHTML = rowData.main.humidity + "%";
-                    row.insertCell(3).innerHTML = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
+                    row.insertCell(3).innerHTML = capitalize(weatherDescription);
                 }  
             }
         
         })
+
+        // Catches any errors with the api request and displays the error message
         .catch(function () {
             warning.style.display = "block";
         });
+}
+
+
+// Check if there are any errors while fetching the api, so the next .then doesn't happen
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+
+// Function to capitalize strings
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);    
 }
